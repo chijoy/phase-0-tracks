bd421b4adcf86d81ac55b98147a8dbe2ac4e5fa0
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-03-27 16:57:52 -0500

    Add questions from Release 1.

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you? What year were you born?"
+age = gets.chomp
+
+puts "Our company cafeterian serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+
+puts "Would you like to enroll  iin the company's health insurance?"
+insurance = gets.chomp
