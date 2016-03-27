7c35fc55ffed3c836947b94e0435febb64a0b986
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-03-27 18:02:46 -0500

    Test first parameter in Release 2.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,11 +1,45 @@
 puts "What is your name?"
 name = gets.chomp
+# p name
 
-puts "How old are you? What year were you born?"
-age = gets.chomp
+puts "How old are you?"
+# tested string versus integer.
+old = gets.chomp.to_i
+# p old
 
-puts "Our company cafeterian serves garlic bread. Should we order some for you?"
+puts "What year were you born?"
+# tested string versus integer
+year = gets.chomp.to_i
+# p year
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 garlic = gets.chomp
+# p garlic
 
-puts "Would you like to enroll  iin the company's health insurance?"
+puts "Would you like to enroll in the company's health insurance?"
 insurance = gets.chomp
+# p insurance
+
+# Determine if year born equals age given.
+
+current_year = 2016
+age = current_year - year
+# #	puts "You are #{age} years old. Congratulations."
+# if age == old
+# #	puts "And you got it right."
+# 	else
+# #	puts "You are a big fat liar."
+# end
+
+
+# set up a parameter for if employee gets age right and is willing to eat garlic bread 
+# and sign up for insurance, not a vampire
+
+if age == old && garlic == "yes" && insurance == "yes"
+	puts "Probably not a vampire."
+else
+	puts "Vampire!"
+end
+
+
+
