8a8e2bc531163468afaf4568c493fd63381b26bf
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-03-30 18:13:20 -0500

    Add more methods.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -51,13 +51,11 @@ puts
 # a = "the usual"
 # a << " suspects"
 
-a = "suspects"
-
-def concat(str)
-	str.concat
+def add_to_str(str,add)
+	str << add
 end
 
-puts concat("the usual")
+puts add_to_str("the usual ", "suspects")
 puts
 
 # #“ suspects”.<???>
@@ -69,18 +67,56 @@ puts
 # “The case of the disappearing last letter”.chop
 # # => “The case of the disappearing last lette”
 
+def chop(str)
+	str.chop
+end
+
+puts chop("The case of the disappearing last letter")
+puts
+
 # “The mystery of the missing first letter”.delete "T"
 # # => “he mystery of the missing first letter”
 
+def delete(str)
+	str.delete "T"
+end
+
+puts delete("The mystery of the missing first letter")
+puts
+
 # “Elementary  ,    my   dear      Watson!”.squeeze
 # # => “Elementary, my dear Watson!”
+
+def squeeze(str)
+	str.squeeze
+end
+
+puts ("Elementary  ,    my   dear      Watson!")
+puts
 
 # “z”.ord
 # # => 122 
 # # (What is the significance of the number 122 in relation to the character z? I had to google 'ordinal number' and I would have to copy and paste the answer from Wikipedia because even after reading the answer, I still couldn't tell you. It's apparently a number that represents the letter, somehow...)
 
+def ord(str)
+	str.ord
+end
+
+puts ord("z")
+puts
+
 # #“How many times does the letter ‘a’ appear in this string?”.<???>
 # #=> 4
 
+"How many times does the letter ‘a’ appear in this string?".count(a)
+
 # a = "How many times does the letter ‘a’ appear in this string?”
 # a.count(a)
+
+# # define a method that counts the occurence of a particular letter in a string
+
+# def count_ltr
+# 	str.count(a)
+# end
+
+# puts count("How many times does the letter a appear in this string?")
