ae54dec043463c7e4de2c663e902149236c76248
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-03-30 17:36:15 -0500

    Add methods.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,45 +4,83 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-“iNvEsTiGaTiOn”.swapcase
+# “iNvEsTiGaTiOn”.swapcase
 # => “InVeStIgAtIoN”
 
-“zom”.insert(1, "o")
-# => “zoom”
+def swap(str)
+	str.swapcase
+end
 
-“enhance”.center(20)
-#=> “    enhance    ”
+puts
+puts swap("iNvEsTiGaTiOn")
+puts
 
-“Stop! You’re under arrest!”.upcase
-# => “STOP! YOU’RE UNDER ARREST!”
+# “zom”.insert(1, "o")
+# # => “zoom”
 
-# “the usual”.<???>
-#=> “the usual suspects”
+def insert(str)
+	str.insert(1,"o")
+end
 
-a = "the usual"
-a << " suspects"
+puts insert("zom")
+puts
 
-#“ suspects”.<???>
-#=> “the usual suspects”
+# “enhance”.center(20)
+# #=> “    enhance    ”
 
-a = " suspects"
-a.prepend("the usual")
+def center(str)
+	str.center(20)
+end
 
-“The case of the disappearing last letter”.chop
-# => “The case of the disappearing last lette”
+puts center("enhance")
+puts
 
-“The mystery of the missing first letter”.delete "T"
-# => “he mystery of the missing first letter”
+# “Stop! You’re under arrest!”.upcase
+# # => “STOP! YOU’RE UNDER ARREST!”
 
-“Elementary  ,    my   dear      Watson!”.squeeze
-# => “Elementary, my dear Watson!”
+def upcase(str)
+	str.upcase
+end
 
-“z”.ord
-# => 122 
-# (What is the significance of the number 122 in relation to the character z? I had to google 'ordinal number' and I would have to copy and paste the answer from Wikipedia because even after reading the answer, I still couldn't tell you. It's apparently a number that represents the letter, somehow...)
+puts upcase("Stop! You’re under arrest!")
+puts
 
-#“How many times does the letter ‘a’ appear in this string?”.<???>
-#=> 4
+# # “the usual”.<???>
+# #=> “the usual suspects”
 
-a = "How many times does the letter ‘a’ appear in this string?”
-a.count(a)
+# a = "the usual"
+# a << " suspects"
+
+a = "suspects"
+
+def concat(str)
+	str.concat
+end
+
+puts concat("the usual")
+puts
+
+# #“ suspects”.<???>
+# #=> “the usual suspects”
+
+# a = " suspects"
+# a.prepend("the usual")
+
+# “The case of the disappearing last letter”.chop
+# # => “The case of the disappearing last lette”
+
+# “The mystery of the missing first letter”.delete "T"
+# # => “he mystery of the missing first letter”
+
+# “Elementary  ,    my   dear      Watson!”.squeeze
+# # => “Elementary, my dear Watson!”
+
+# “z”.ord
+# # => 122 
+# # (What is the significance of the number 122 in relation to the character z? I had to google 'ordinal number' and I would have to copy and paste the answer from Wikipedia because even after reading the answer, I still couldn't tell you. It's apparently a number that represents the letter, somehow...)
+
+# #“How many times does the letter ‘a’ appear in this string?”.<???>
+# #=> 4
+
+# a = "How many times does the letter ‘a’ appear in this string?”
+# a.count(a)
