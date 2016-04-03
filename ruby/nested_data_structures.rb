e26128dbcd23cbc3bd6858410c00de76d9e1484a
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-04-03 14:55:35 -0500

    Initialize assignment 5.5 release 2.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,28 @@
+# assignment 5.5 release 2
+# design and build a nested data structure for an office building
+# the building is the hash, 
+# the offices are an array
+
+building = {
+	coffee_house: {
+		name: "Starbucks",
+		menu: [
+			"coffee",
+			"pastries",
+			"chocolate"
+		]
+	}
+	# retail_store: {
+	# 	name: "Staples",
+	# 	for_sale: [
+	# 		"printers",
+	# 		"paper",
+	# 		"pens"
+	# 	]
+	# }
+
+}
+
+p building
+# p building[coffee_house]
+# p building[coffee_house][0]
