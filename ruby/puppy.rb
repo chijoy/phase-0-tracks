5ad43eae4960a8278d4e3e10378ee43ba970f99c
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-04-05 12:24:20 -0500

    Initialize for assignment.

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+# SPECIES
+# Man's best friend
+
+# CHARACTERISTICS
+# size: varies
+# color: varies
+# name: varies
+# eye count: 2
+# legs: 4
+# tail: 1
+# playfullness: varies
+# loveability: high
+
