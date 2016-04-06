10dfbc70de8fadf3f4987d93d762a031c77f3375
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-04-06 16:02:21 -0500

    Do the second thing in release 1.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,23 +1,55 @@
 # Create a Santa class
 
 class Santa
+
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+	end
+
+	def speak
+			puts "Ho, ho, ho! Haaaaappy Holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That was a good #{cookie}!"
+	end
+
+	def about
+	  puts "Gender: #{@gender}"
+	  puts "Ethnicity: #{@ethnicity}"
+	end
+
+	# reindeer_ranking = [
+	# 	"Rudolph",
+	# 	"Dasher",
+	# 	"Dancer",
+	# 	"Prancer",
+	# 	"Vixen",
+	# 	"Comet",
+	# 	"Cupid",
+	# 	"Donner",
+	# 	"Blitzen"
+	# ]
+
+	# age = 0
+
 end
 
-def speak
-		puts "Ho, ho, ho! Haaaaappy Holidays!"
-end
+puts
+santa = Santa.new("female", "Caucasian")
+santa.about
+santa.speak
+santa.eat_milk_and_cookies("snickerdoodle")
+puts
+santa = Santa.new("male", "Italian")
+santa.about
+santa.speak
+santa.eat_milk_and_cookies("snickerdoodle")
+puts
+# p reindeer_ranking
 
-def eat_milk_and_cookies(cookie)
-	puts "That was a good #{cookie}!"
-end
-
-def initialize 
-	puts "Initializing Santa instance..."
-end
-
-
-puts speak
-puts eat_milk_and_cookies("snickerdoodle")
-puts initialize
-
-
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
