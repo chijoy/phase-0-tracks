fdd352290ef5db586f004ae7705fa0c2223f03c5
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-04-06 15:22:34 -0500

    Release 0, Do the thing completed.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,10 +1,23 @@
 # Create a Santa class
 
 class Santa
-
-	def speak
-		puts "Ho, ho, ho! Haaaaappy Holidays!"
-	end
 end
 
+def speak
+		puts "Ho, ho, ho! Haaaaappy Holidays!"
+end
+
+def eat_milk_and_cookies(cookie)
+	puts "That was a good #{cookie}!"
+end
+
+def initialize 
+	puts "Initializing Santa instance..."
+end
+
+
 puts speak
+puts eat_milk_and_cookies("snickerdoodle")
+puts initialize
+
+
