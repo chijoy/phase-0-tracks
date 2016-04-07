4f217e9b22db8110241528ba2d2db4a76b281ab6
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-04-07 18:30:47 -0500

    Release 2.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,20 +2,43 @@
 
 class Santa
 
+# define an initialize method
+# output: "Initializing Santa instance..."
+
 	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+
+# add gender attribute
+
 		@gender = gender
+
+# add ethnicity attribute
+
 		@ethnicity = ethnicity
+
+# add age attribute
+
 		@age = "0"
 	end
+
+# define a speak method 
+# input "Ho, ho, ho! Haaaaappy Holidays!"
+# output "Ho, ho, ho! Haaaaappy Holidays!"
 
 	def speak
 			puts "Ho, ho, ho! Haaaaappy Holidays!"
 	end
 
+# define an eat_milk_and_cookies method
+# input type of cookies
+# output: takes the type of cookie and inserts it into
+# "That was a good (cookie)!"
+
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
+
+# define an about method that puts class attributes
 
 	def about
 	  puts "Gender: #{@gender}"
@@ -23,17 +46,17 @@ class Santa
 	  puts "Age: #{@age}"
 	end
 
-	# reindeer_ranking = [
-	# 	"Rudolph",
-	# 	"Dasher",
-	# 	"Dancer",
-	# 	"Prancer",
-	# 	"Vixen",
-	# 	"Comet",
-	# 	"Cupid",
-	# 	"Donner",
-	# 	"Blitzen"
-	# ]
+	@reindeer_ranking = [
+		"Rudolph",
+		"Dasher",
+		"Dancer",
+		"Prancer",
+		"Vixen",
+		"Comet",
+		"Cupid",
+		"Donner",
+		"Blitzen"
+	]
 
 end
 
@@ -48,7 +71,7 @@ santa.about
 santa.speak
 santa.eat_milk_and_cookies("Take5")
 puts
-# p reindeer_ranking
+p reindeer_ranking
 
 santas = []
 santas << Santa.new("ambiguous", "NA")
