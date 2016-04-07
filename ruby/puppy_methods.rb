dfa59032e70ee9e8df5fb8f8e4a64f3c1aa48c75
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-04-06 21:21:24 -0500

    Release 0 and Release 1 for pair assignment.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,30 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
+  
+  def speak
+    3.times {|bark_count| puts "Woof!" }
+  end
+  
+  def roll_over
+    puts "Roll over!"
+  end
+  
+  # age multiply by 7 to get dog yeaars
+  def dog_years(x)
+    puts x * 7
+  end
+  
+def play_dead
+  puts "Lays down dead... :("
+end
+  
 
 end
 
+puppy = Puppy.new
+puppy.fetch("ball")
+puppy.speak
+puppy.roll_over
+puppy.dog_years(4)
+puppy.play_dead
