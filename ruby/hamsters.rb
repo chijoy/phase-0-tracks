2ba952b677feef341d5efee8098b1fdec414cc2b
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-04-14 11:46:03 -0500

    Begin assignment. Ask designated questions.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,1 +1,10 @@
-
+puts "What is the hamster's name?"
+name = gets.chomp
+puts "What is the volume level from 1 to 10?"
+volume = gets.chomp.to_i
+puts "What is the fur color?"
+color = gets.chomp
+puts "Is the hamster a good candidate for adoption?"
+candidate = gets.chomp
+puts "What is the estimated age of your hamster?"
+age = gets.chomp.to_i
