3490915cc91b531bd0d7c49c7e88744803ec7ccf
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-04-18 11:13:36 -0500

    Finish allergies.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,13 +4,17 @@
 # start the survey at 0, so that if they enter one, the loop will run one time
 # count each time the survey is taken.
 # Repeat until all employees have been processed using until loop
-# so if the number is 2, repeat the survey until number == 2
+# so if the employees number is 2, repeat the survey until number == 2
 puts "How many employees will be processed?"
-number = gets.chomp.to_i
-p number
+employees = gets.chomp.to_i
+p employees
 
 index = 0
-until number
+while index < employees
+
+	# index = 0
+# while index < employees
+# 	index += 1
 
 
 	# Write a program that asks the new employee the following questions,
@@ -110,79 +114,26 @@ until number
 		puts
 		puts "Results inconclusive."
 	end
+end
+
+puts "Please name any allergies that you have, one at a time, then type 'Done.'"
+puts "If no allergies, just type 'Done.'"
+allergies = gets.chomp.downcase
+
+# Ask employees if they have allergies
+# if they type sunshine, puts Probably a vampire
+# instruct them to type done, when done
+# when they type done, end the loop
+until allergies == "done" do
+	if allergies == "sunshine"
+		puts "Probably a vampire."
+	break
+	end
+end
 
 	puts
 	puts "The result for #{name} is: #{reply}"
 
-
-
-# puts "How many employees will be processed?"
-# employees = gets.chomp.to_i
-
-# index = 0
-# while index < employees
-# 	index += 1
-
-# 	puts "What is your name?"
-# 	name = gets.chomp
-# 	# p name
-
-# 	puts "How old are you?"
-# 	# test string versus integer.
-# 	old = gets.chomp.to_i
-# 	# p old
-
-# 	puts "What year were you born?"
-# 	# test string versus integer
-# 	year = gets.chomp.to_i
-# 	# p year
-
-# 	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-# 	garlic = gets.chomp.downcase
-# 	# p garlic
-
-# 	puts "Would you like to enroll in the company's health insurance?"
-# 	insurance = gets.chomp.downcase
-# 	# p insurance
-
-# 	puts "Please name any allergies that you have, one at a time, then type 'Done.'"
-# 	puts "If no allergies, just type 'Done.'"
-# 	allergies = gets.chomp.downcase
-
-# # Ask employees if they have allergies, one at a time, until they type done, unless they type sunshine.
-# 	until allergies == "done" do
-# 		if allergies == "sunshine"
-# 			puts "Probably a vampire."
-# 		break
-# 		end
-# 	end
-
-
-# 	# Determine if year born equals age given.
-# 	current_year = 2016
-# 	age = current_year - year
-
-# 	# Set up a condition for if employee gets age right and is willing to eat garlic bread 
-# 	# and sign up for insurance, not a vampire
-# 	if age == old && garlic == "yes" && insurance == "yes"
-# 		puts "Probably not a vampire."
-# 	# Set up a condition if employee gets age wrong and is not willing to eat garlic bread 
-# 	# or is not willing to sign up for insurance, probably a vampire
-# 	elsif age != old && (garlic == "no" || insurance == "no")
-# 		puts "Probably a vampire."
-# 	# Set up a condition if employee gets age wrong and is not willing to eat garlic bread 
-# 	# and is not willing to sign up for insurance, almost certainly a vampire
-# 	elsif age != old && (garlic == "no" && insurance == "no")
-# 		puts "Almost certainly a vampire."
-# 	# Set up a condition if the employee gives the Drake Cula or Tu Fang, definitely a vampire.
-# 	elsif name == "Drake Cula" || name == "Tu Fang"
-# 	 	puts "Definitely a vampire."
-# 	 # Set up a condition if none of the other conditions are met, results inconclusive.
-# 	 else
-# 	 	puts "Results inconclusive."
-# 	end
-# end
-
-# puts
-# puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
-
+puts
+puts "Actually, never mind! What do these questions have to do with anything?"
+puts "Let's all be friends."
