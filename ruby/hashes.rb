9d30bbd177e5ee11f918b701089ac96d89c9348e
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-04-20 15:13:17 -0500

    Prompt designer for client input.

---
+++ ruby/hashes.rb
@@ -0,0 +1,24 @@
+# pseudocode and write a program that will allow an interior designer 
+# to enter the details of a given client.
+
+# Prompt the designer for client's name, age, number of children, decor theme.
+puts
+puts "What is the client's first name?"
+first_name = gets.chomp.capitalize
+
+puts
+puts "What is the client's last name?"
+last_name = gets.chomp.capitalize
+
+puts
+puts "What is the client's age?"
+age = gets.chomp.to_i
+
+puts
+puts "How many children does the client have?"
+children = gets.chomp.to_i
+
+puts
+puts "What is the client's decor theme?"
+theme = gets.chomp.to_i
+
