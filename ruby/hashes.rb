5515ffa4accd7c3d0f9be5d6a86908833c53c993
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-04-20 16:23:09 -0500

    Add ability for designer to make changes.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -37,28 +37,66 @@ client[:age] = age
 # Prompt the designer for the number of children the client has
 puts
 puts "How many children does the client have?"
-children = gets.chomp.to_i
+number_of_children = gets.chomp.to_i
 # p children
 
 # Add children to client hash
-client[:children] = children
+client[:number_of_children] = number_of_children
 # p client
 
 # Prompt the designer for their decor theme
 puts
 puts "What is the client's decor theme?"
-theme = gets.chomp.capitalize
+decor_theme = gets.chomp.capitalize
 # p theme
 
 # Add decor theme to client hash
-client[:theme] = theme
+client[:decor_theme] = decor_theme
 # p client
 
-# Print the hash back out to the screen
+# Print the hash back out to the screen in a print friendly version
 puts
 puts "Here is your client's information:"
 puts "First name: #{first_name}"
 puts "Last name: #{last_name}"
 puts "Age: #{age}"
-puts "Number of children: #{children}"
-puts "Decor theme: #{theme}"
+puts "Number of children: #{number_of_children}"
+puts "Decor theme: #{decor_theme}"
+
+# ask if any changes need to be made
+# if none, instruct the designer to enter none
+# otherwise, instruct the designer to enter the label that needs to be changedd
+puts "Do any changes need to be made?"
+puts "If not, type none, otherwise, enter the label that needs to be updated."
+reply = gets.chomp
+	if reply == "none"
+		puts "Thank you!"
+	elsif reply == "first name"
+		puts "Enter the first name."
+		new_first_name = gets.chomp.capitalize
+		client[first_name] = new_first_name
+		puts "The client's first name has been updated to #{new_first_name}."
+	elsif reply == "last name"
+		puts "Enter the last name."
+		new_last_name = gets.chomp.capitalize
+		client[last_name] = new_last_name
+		puts "The client's last name has been updated to #{new_last_name}."
+	elsif reply == "age"
+		puts "Enter the age."
+		new_age = gets.chomp.to_i
+		client[age] = new_age
+		puts "The client's age has been updated to #{new_age}."
+	elsif reply == "number of children"
+		puts "Enter the number of children."
+		new_number_of_children = gets.chomp.to_i
+		client[number_of_children] = new_number_of_children
+		puts "The client's number of children has been updated to #{new_number_of_children}."
+	elsif reply == "decor theme"
+		puts "Enter the decor theme."
+		new_decor_theme = gets.chomp.capitalize
+		client[decor_theme] = new_decor_theme
+		puts "The client's decor theme has been updated to #{new_decor_theme}."
+	else
+		puts "Sorry, I didn't understand."
+	end
+
