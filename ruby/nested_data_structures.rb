2efb47d1507f95d3a7ddf0e3ac40c49a44a5b7d1
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-04-22 14:52:00 -0500

    Create a nested data structure.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,17 @@
+school = {
+	first_grade_classroom: {
+		name: "First Grade Classroom",
+		number_of_students: 10,
+		recess: true
+	},
+	second_grade_classroom: {
+		name: "Second Grade Classroom"
+		number_of_students: 12,
+		recess: true
+	},
+	third_grade_classroom: {
+		name: "Third Grade Classroom",
+		number_of_students: 8,
+		recess: false
+	}
+}
