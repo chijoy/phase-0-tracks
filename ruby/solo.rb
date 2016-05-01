c7c45edfcf8a8766771d8a79cc5fac7a3f4e7b23
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-05-01 11:38:07 -0500

    Add three methods.

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -3,7 +3,7 @@
 # include at least three attributes
 # 	1. name
 # 	2. gender
-# 	3. age
+# 	3. color
 # include at least three methods
 # 	1. vacuum floor
 # 	2. mop floor
@@ -11,15 +11,23 @@
 
 
 class Robot
-	def initialize(name, gender, age)
+	def initialize(name, gender, color)
 		puts
 		puts "Initialize Robot instance..."
 		@name = name
 		@gender = gender
-		@age = age
+		@color = color
 	endm
 
-	def vacuum_floor
+	def vacuum_floor(room)
+		puts "Please vacuum the #{room} now."
+	end
 
+	def mop_floor(room)
+		puts "Please mop the #{room} now."
+	end
 
+	def walk_dog(dog)
+		puts "Please walk #{dog} now."
+	end
 end
