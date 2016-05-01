2db3fc1179e3b16012c98ae5ce8e8f898336d47c
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-05-01 11:33:02 -0500

    Create a Robot class, add three attributes.

---
+++ ruby/solo.rb
@@ -0,0 +1,25 @@
+# Define your own class
+# 	Make a Robot class
+# include at least three attributes
+# 	1. name
+# 	2. gender
+# 	3. age
+# include at least three methods
+# 	1. vacuum floor
+# 	2. mop floor
+# 	3. walk dogs
+
+
+class Robot
+	def initialize(name, gender, age)
+		puts
+		puts "Initialize Robot instance..."
+		@name = name
+		@gender = gender
+		@age = age
+	endm
+
+	def vacuum_floor
+
+
+end
