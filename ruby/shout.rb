6b87a7b7a5a882306c6b1a20f04b8a6ba663ee51
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-05-01 21:05:17 -0500

    Add driver code and test.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -5,8 +5,13 @@ module Shout
 		words + "!!!" + " :("
 	end
 
-	# add a yelling happily mehod
+	# add a yelling happily method
 	def self.yelling_happily(happy_words)
 		happy_words + "!!!" + " :)"
 	end
 end
+
+# Driver Code
+
+p Shout.yell_angrily("I hate procrastinating")
+p Shout.yelling_happily("I love it when I get it right")
