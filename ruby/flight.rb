cb9075076c9879f12ceac7e229f89f9f9732be41
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-05-01 21:07:56 -0500

    Add module to classes.

--- ruby/flight.rb
+++ ruby/flight.rb
@@ -5,14 +5,18 @@ module Flight
 	end
 end
 
-
-
 # declare Bird class
+# add module to class
+# include <module name>
 class Bird
+	include Flight
 end
 
 # declass Plane class
+# add module to class
+# include <module name>
 class Plane
+	include Flight
 end
 
 
