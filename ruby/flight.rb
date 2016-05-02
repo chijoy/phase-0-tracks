e2c643f6a8fd1918636f9d66f51a98a9f9a5af5b
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-05-01 21:05:50 -0500

    Declare Bird and Plane class, declare Flight module.

---
+++ ruby/flight.rb
@@ -0,0 +1,18 @@
+# write a Flight module
+module Flight
+	def take_off(altitude)
+		puts "Taking off and ascending until reaching #{altitude} ..."
+	end
+end
+
+
+
+# declare Bird class
+class Bird
+end
+
+# declass Plane class
+class Plane
+end
+
+
