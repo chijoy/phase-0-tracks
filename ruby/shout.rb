df3af87bb3787a0da7f519141abf84a72a3704ac
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-05-01 21:19:53 -0500

    Conver to mixin module.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,17 +1,42 @@
 # Declare module
 
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	# add a yelling happily method
+# 	def self.yelling_happily(happy_words)
+# 		happy_words + "!!!" + " :)"
+# 	end
+# end
+
+# Driver Code
+# p Shout.yell_angrily("I hate procrastinating")
+# p Shout.yelling_happily("I love it when I get it right")
+
+# and now we start over...
+# convert standalone module to a mixin
+# write two classes representing anything that might shout
+
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	def self.yell_happy(happy_words)
+		words + "Yahoo!"
 	end
 
-	# add a yelling happily method
-	def self.yelling_happily(happy_words)
-		happy_words + "!!!" + " :)"
+	def self.yell_mad(mad_words)
+		words + "Nooooooooooooooooo!"
 	end
 end
 
-# Driver Code
+class Fans
+	include Shout
+end
 
-p Shout.yell_angrily("I hate procrastinating")
-p Shout.yelling_happily("I love it when I get it right")
+class Toddlers
+	include Shout
+end
+
+# test work
+fans = Fans.new
+fans.yell_happy("Cubs win!")
