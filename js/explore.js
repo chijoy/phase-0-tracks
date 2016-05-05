de545da3dbf1c52d9805bc90ea8c6e11c51ec8e3
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-05-05 11:00:40 -0500

    Pseudocode a function that takes a string and reverses the string.

---
+++ js/explore.js
@@ -0,0 +1,22 @@
+// A function that takes a string as a parameter and reverses the string.
+// Create a new string
+// Add to the new string
+// Return the reversed string
+// input: "hello"
+// output: "olleh"
+
+// create string for hello
+var string = "hello";
+// create a new empty string for reversed string
+var reversedString = ""
+
+// use a loop to go hrough the string
+// i is the index, the length of the string
+
+for (i = string.lenth) -1; i >= 0; i --) {
+	reversedString += string[i]
+}
+
+
+// DRIVER CODE
+
