9b38b5e955e1e58f798b319d5aebbf40c35217a7
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-05-05 11:19:58 -0500

    Add loop.

--- js/explore.js
+++ js/explore.js
@@ -13,10 +13,26 @@ var reversedString = ""
 // use a loop to go hrough the string
 // i is the index, the length of the string
 
-for (i = string.lenth) -1; i >= 0; i --) {
+for (i = string.lenth -1; i >= 0; i --) {
 	reversedString += string[i]
+}
+
+
+console.log(reversedString)
+
+function reverse(string) {
+	var reversedString = ""
+	for (i = string.length -1; i >= 0; i --)
+		{reversedString += string[i]}
+	return reversedString
 }
 
 
 // DRIVER CODE
 
+var testString = reverse("hello");
+var testString = reverse("hi");
+
+if (testString.length > 3) {
+	console.log(testString);
+} else {console.log("The string is too short.");}
