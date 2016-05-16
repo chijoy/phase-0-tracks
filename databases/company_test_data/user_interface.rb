9e91bb4575d08685f5774fff95c448abba345225
Joy McCaffrey <joymccaffreychicago@gmail.com>
2016-05-16 15:33:05 -0500

    Add user interfacee.

---
+++ databases/company_test_data/user_interface.rb
@@ -0,0 +1,4 @@
+require_relative "company_test_data"
+# require "sqlite3"
+
+puts "Hello! Welcome to our test company program.\nWould you like to create a new customer?\nOr view test customers?\nEnter 'Create' or 'View' now."
